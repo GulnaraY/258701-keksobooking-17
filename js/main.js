7fecfcd15a169c0e7f90c66555e3c5b6c7d8b87f
GulnaraY <guga.ufa@gmail.com>
2019-06-18 17:51:42 +0300

    валидация формы

--- js/main.js
+++ js/main.js
@@ -1,15 +1,25 @@
 'use strict';
 
 var BOOKING_OBJECTS_COUNT = 8;
-var HOUSING_TYPES = ['place', 'flat', 'house', 'bungalo'];
+var HOUSING_TYPES = ['palace', 'flat', 'house', 'bungalo'];
 var PIN_WIDTH = 50;
 var PIN_HEIGHT = 70;
-var MIN_X = 0 + PIN_WIDTH / 2;
+var MIN_X = PIN_WIDTH / 2;
 var MAX_X = 1200 - PIN_WIDTH / 2;
 var MIN_Y = 130;
 var MAX_Y = 630;
 var map = document.querySelector('.map');
 var pin = document.querySelector('#pin').content.querySelector('.map__pin');
+var typeInput = document.querySelector('#type');
+var priceInput = document.querySelector('#price');
+var timeInInput = document.querySelector('#timein');
+var timeOutInput = document.querySelector('#timeout');
+var HOUSING_TYPES_PRICESES = {
+  'palace': 10000,
+  'flat': 1000,
+  'house': 5000,
+  'bungalo': 0
+};
 
 var activateMap = function () {
   map.classList.remove('map--faded');
@@ -60,5 +70,36 @@ var swowSimilarOffers = function () {
   map.appendChild(fragment);
 };
 
+var compareTypeAndPrice = function (housingType) {
+  var housingMinPrice = HOUSING_TYPES_PRICESES[housingType];
+  priceInput.min = housingMinPrice;
+  priceInput.placeholder = housingMinPrice;
+};
+
 swowSimilarOffers();
 activateMap();
+
+var onFormTypeClick = function (evt) {
+  compareTypeAndPrice(evt.target.value);
+};
+
+typeInput.addEventListener('click', onFormTypeClick);
+
+var setTimeIn = function (time) {
+  timeInInput.value = time;
+};
+
+var setTimeOut = function (time) {
+  timeOutInput.value = time;
+};
+
+var onFormTimeClick = function (evt) {
+  if (evt.target === timeInInput) {
+    setTimeOut(evt.target.value);
+  } else {
+    setTimeIn(evt.target.value);
+  }
+};
+
+timeInInput.addEventListener('click', onFormTimeClick);
+timeOutInput.addEventListener('click', onFormTimeClick);
