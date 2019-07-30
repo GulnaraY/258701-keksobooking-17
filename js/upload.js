a150c6a66ccf680c5a8083945384e6c891985b56
GulnaraY <guga.ufa@gmail.com>
2019-07-30 10:27:56 +0300

    Устраняет ошибку при загрузке фото

--- js/upload.js
+++ js/upload.js
@@ -13,52 +13,56 @@
 
   fileChooserAvatar.addEventListener('change', function () {
     var file = fileChooserAvatar.files[0];
-    var fileName = file.name.toLowerCase();
+    if (file) {
+      var fileName = file.name.toLowerCase();
 
-    var matches = FILE_TYPES.some(function (element) {
-      return fileName.endsWith(element);
-    });
-
-    if (matches) {
-      var reader = new FileReader();
-
-      reader.addEventListener('load', function () {
-        previewAvatar.src = reader.result;
+      var matches = FILE_TYPES.some(function (element) {
+        return fileName.endsWith(element);
       });
 
-      reader.readAsDataURL(file);
+      if (matches) {
+        var reader = new FileReader();
+
+        reader.addEventListener('load', function () {
+          previewAvatar.src = reader.result;
+        });
+
+        reader.readAsDataURL(file);
+      }
     }
   });
 
   fileChooserPhoto.addEventListener('change', function () {
 
     var file = fileChooserPhoto.files[0];
-    var fileName = file.name.toLowerCase();
+    if (file) {
+      var fileName = file.name.toLowerCase();
 
-    var matches = FILE_TYPES.some(function (element) {
-      return fileName.endsWith(element);
-    });
-
-    if (matches) {
-      var reader = new FileReader();
-
-      reader.addEventListener('load', function () {
-        if (!isFirstPhotoChoosing) {
-          var img = document.createElement('img');
-          img.style.width = '100%';
-          img.style.height = '100%';
-          img.src = reader.result;
-          priviewPhotoContainer.insertAdjacentElement('afterbegin', img);
-          isFirstPhotoChoosing = true;
-        } else {
-          var priviewContainer = priviewPhotoContainer.cloneNode(true);
-          var priview = priviewContainer.querySelector('img');
-          priview.src = reader.result;
-          photosContainer.appendChild(priviewContainer);
-        }
+      var matches = FILE_TYPES.some(function (element) {
+        return fileName.endsWith(element);
       });
 
-      reader.readAsDataURL(file);
+      if (matches) {
+        var reader = new FileReader();
+
+        reader.addEventListener('load', function () {
+          if (!isFirstPhotoChoosing) {
+            var img = document.createElement('img');
+            img.style.width = '100%';
+            img.style.height = '100%';
+            img.src = reader.result;
+            priviewPhotoContainer.insertAdjacentElement('afterbegin', img);
+            isFirstPhotoChoosing = true;
+          } else {
+            var priviewContainer = priviewPhotoContainer.cloneNode(true);
+            var priview = priviewContainer.querySelector('img');
+            priview.src = reader.result;
+            photosContainer.appendChild(priviewContainer);
+          }
+        });
+
+        reader.readAsDataURL(file);
+      }
     }
   });
   /**
